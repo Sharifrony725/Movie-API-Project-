023f5d1573ce25315ba43d9a34060fc289be4520
Shariful Islam <sharifrony725@gmail.com>
2022-02-17 15:48:03 +0600

    17_2_22_api_for_movie

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,11 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Movie;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Category extends Model
 {
     use HasFactory;
     protected $guarded = [];
+    public function movies(): BelongsToMany
+    {
+        return $this->belongsToMany(Movie::class, 'category_movies');
+    }
 }
--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -2,11 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Movie extends Model
 {
     use HasFactory;
     protected $guarded = [];
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class, 'category_movies');
+    }
 }
